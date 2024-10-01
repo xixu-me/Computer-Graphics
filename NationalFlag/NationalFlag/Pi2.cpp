ddce66265da1d05f193a635b79142890f9c299a3
Xi Xu <i@xi-xu.me>
2024-10-01 21:04:09 +0800

    Create Pi2.cpp

---
+++ NationalFlag/NationalFlag/Pi2.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "Pi2.h"
+
+CPi2::CPi2() {
+	this->x = 0.0;
+	this->y = 0;
+	this->c = CRGB(0.0, 1.0, 1.0);
+}
+
+CPi2::~CPi2() {
+}
+
+CPi2::CPi2(double x, int y) {
+	this->x = x;
+	this->y = y;
+}
